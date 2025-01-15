50d4aa9bd68390ab06838191a6a65efd397a9924
fmot <genzzabro09812@gmail.com>
2025-01-15 06:43:12 -0800

    2-20 useLockBodyScroll

--- components/ui/mobile-nav.tsx
+++ components/ui/mobile-nav.tsx
@@ -1,5 +1,6 @@
 import { siteconfig } from "@/config/site";
 import { NavItem } from "@/types";
+import { useLockBodyScroll } from "@uidotdev/usehooks";
 import Link from "next/link";
 
 interface MobileNavProps {
@@ -8,18 +9,20 @@ interface MobileNavProps {
 
 
 export default function MobileNav({items}: MobileNavProps){
-    return(
-        <div className="fixed top-16 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
-            <div>
-                <Link href={"/"}>
-                  {siteconfig.name}
-                </Link>
-                <nav>
-                    {items.map((item, index) =>(
-                        <Link key={index} href={item.href}>{item.title}</Link>
-                    ))}
-                </nav>
-            </div>
-        </div>
-    )
+  useLockBodyScroll();
+
+  return(
+      <div className="fixed top-16 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
+          <div>
+              <Link href={"/"}>
+                {siteconfig.name}
+              </Link>
+              <nav>
+                  {items.map((item, index) =>(
+                      <Link key={index} href={item.href}>{item.title}</Link>
+                  ))}
+              </nav>
+          </div>
+      </div>
+  )
 }
