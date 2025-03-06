74725ce59f81394ef3e7e4d4194810268d61d1eb
fmot <genzzabro09812@gmail.com>
2025-03-05 16:15:24 -0800

    6-67

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import DashBoardHeader from "@/components/dashboard-header";
 import DashBoardShell from "@/components/dashboard-shell";
 
 export default function DashboardPage() {
   return (
     <DashBoardShell>
-      <div>Dashboard</div>
+      <DashBoardHeader heading="記事投稿" text="記事の投稿と管理">
+        Create
+      </DashBoardHeader>
     </DashBoardShell>
   );
 }
