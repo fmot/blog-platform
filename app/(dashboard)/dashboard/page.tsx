a8f848fd64a2547a1734e10ba2010ad6ba44e552
fmot <genzzabro09812@gmail.com>
2025-03-06 15:55:17 -0800

    6-68

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import DashBoardHeader from "@/components/dashboard-header";
 import DashBoardShell from "@/components/dashboard-shell";
+import PostCreateButton from "@/components/post-create-button";
 
 export default function DashboardPage() {
   return (
     <DashBoardShell>
       <DashBoardHeader heading="記事投稿" text="記事の投稿と管理">
-        Create
+        <PostCreateButton />
       </DashBoardHeader>
     </DashBoardShell>
   );
---
+++ components/post-create-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ButtonProps, buttonVariants } from "./ui/button";
+import { useState } from "react";
+import { Icon } from "./icon";
+
+interface PostCreateButtonProps extends ButtonProps {}
+
+export default function PostCreateButton({
+  className,
+  variant,
+  ...props
+}: PostCreateButtonProps) {
+  const [isLoading, setIsLoafing] = useState(false);
+
+  const onClick = async () => {};
+
+  return (
+    <button
+      className={cn(
+        buttonVariants({ variant }),
+        { "cursor-not-allowed opacity-60": isLoading },
+        className
+      )}
+      onClick={onClick}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <Icon.spinner className="animate-spin mr-2 h-4 w-4" />
+      ) : (
+        <Icon.add className="mr-2 h-4 w-4" />
+      )}
+      新しい投稿
+    </button>
+  );
+}
