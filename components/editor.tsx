990bca3240062759045a0e2cebbd891d7fb7c0a9
fmot <genzzabro09812@gmail.com>
2025-03-10 06:48:10 -0700

    7-80 81

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,11 +1,38 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import TextAreaAutoSize from "react-textarea-autosize";
+import EditorJS from "@editorjs/editorjs";
+import { useEffect, useState } from "react";
 
 export default function Editor() {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  const initializeEditor = async () => {
+    const editor = new EditorJS({
+      holder: "editor",
+      placeholder: "Start writing your post",
+      inlineToolbar: true,
+    });
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      initializeEditor();
+    }
+  }, [isMounted]);
+
   return (
     <form>
-      <div>
+      <div className="grid w-full gap-10">
         <div className="flex w-full items-center justify-between">
           <div className="flex items-center space-x-10">
             <Link
@@ -21,13 +48,14 @@ export default function Editor() {
           </button>
         </div>
         <div>
-          <textarea
+          <TextAreaAutoSize
             id="title"
             autoFocus
             placeholder="Post Title"
             className="w-full resize-none overflow-hidden bg-transparent text-5xl focus:outline-none font-bold"
-          ></textarea>
+          />
         </div>
+        <div id="editor" className="min-h-[500px]" />
         <p className="test-sm text-gray-500">
           Use
           <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">
